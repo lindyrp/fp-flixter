e356a2118176a5ff78b671e3aca06e520228796c
Lindy Sinclair <lindyrp@gmail.com>
2016-05-10 22:29:20 +0000

    Build lessons

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Flixter::Application.routes.draw do
 
   resources :courses, only: [:index, :show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:new, :create] do
+      end
     end
   end
 
