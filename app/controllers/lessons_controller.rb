59ea0a52358647504f70e6f9d16b90f2527e7581
Lindy Sinclair <lindyrp@gmail.com>
2016-05-19 23:56:14 +0000

    Add Enrollment Authentication to Lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson
+
   def show
   end
 
   private
+
+  def require_enrollment_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      redirect_to course_path(current_lesson.section.course), alert: 'Please Enroll to See This Lesson'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
